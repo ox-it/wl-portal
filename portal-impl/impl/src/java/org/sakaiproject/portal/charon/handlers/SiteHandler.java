6712271553da8dc355263a0c744b27ca28e752f8
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2009-02-08 22:20:48 +0000

    WL-409 Don't display My Active Site before login

--- portal-impl/impl/src/java/org/sakaiproject/portal/charon/handlers/SiteHandler.java
+++ portal-impl/impl/src/java/org/sakaiproject/portal/charon/handlers/SiteHandler.java
@@ -640,7 +640,7 @@ public class SiteHandler extends WorksiteHandler
 				}
 			}
 
-			rcontext.put("useDHTMLMore", useDHTMLMore);
+			rcontext.put("useDHTMLMore", useDHTMLMore && loggedIn);
 			if (useDHTMLMore)
 			{
 				SiteView siteView = portal.getSiteHelper().getSitesView(
