512378a5a429eb34de6af6aa07e7a8e9424388eb
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2009-06-22 10:21:10 +0100

    WL-721 Don't insert newlines in short description.

--- portal-impl/impl/src/java/org/sakaiproject/portal/charon/site/PortalSiteHelperImpl.java
+++ portal-impl/impl/src/java/org/sakaiproject/portal/charon/site/PortalSiteHelperImpl.java
@@ -337,7 +337,7 @@ public class PortalSiteHelperImpl implements PortalSiteHelper
 		// TODO Description contains HTML now so is this sensible?
 		m.put("siteDescription", Web.escapeHtml(s.getDescription()));
 		// Short description shouldn't have HTML so should be fine.
-		m.put("shortDescription", Web.escapeHtml(s.getShortDescription()));
+		m.put("shortDescription", Web.escapeHtml(s.getShortDescription(), false));
 		String siteUrl = Web.serverUrl(req)
 				+ ServerConfigurationService.getString("portalPath") + "/";
 		if (prefix != null) siteUrl = siteUrl + prefix + "/";
